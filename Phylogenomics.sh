6b9e95e9659d477dbac255d214d1dc77a980b500
mariebuysse <58982033+mariebuysse@users.noreply.github.com>
2021-06-30 13:10:16 +0200

    Update Phylogenomics.sh

--- Phylogenomics.sh
+++ Phylogenomics.sh
@@ -1,1 +1,4 @@
-
+orthofinder -f ./symbiont_phylo -t 4 -S blast ##symbiont_phylo: directory containing all genomes of FLE or Midichloria or Rickettsia used for the phylogenomic analyses 
+# ADD command line MUSCLE and Gblocks
+modeltest-ng -i symbiont-phylo_concatenated.faa -p 12 -T raxml -d aa
+raxmlHPC-PTHREADS -s Selected_AlnGblock.fasta -m model -n Selected_AlnGblock.fasta.boot -x 1234 -p 123 -f a -T 12 -# 100 
