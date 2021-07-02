ceeac44c8bb9717e3f0fd3433c547a487f086929
tiagonardi <33203173+tiagonardi@users.noreply.github.com>
2021-07-02 17:52:41 +0200

    Create split_cogtab.py

---
+++ split_cogtab.py
@@ -0,0 +1,11 @@
+# Use as: python3 split_cogtab.py orthogroups.tsv <GENOME>
+import sys
+import re
+genome = sys.argv[2]
+orthofile = sys.argv[1]
+f = open(orthofile)
+for line in f:
+	linea=line.strip().split("\t")
+	for l in linea[1:]:
+		if l == genome:
+			print (linea[0])
