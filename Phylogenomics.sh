5971541d4d0dc0be54b82ab72e9708c1ae2a030f
annamariafloriano <86775791+annamariafloriano@users.noreply.github.com>
2021-07-12 15:53:08 +0200

    Update Phylogenomics.sh

--- Phylogenomics.sh
+++ Phylogenomics.sh
@@ -1,7 +1,14 @@
 orthofinder -f ./symbiont_phylo -t 4 -S blast ##symbiont_phylo: directory containing all genomes of FLE or Midichloria or Rickettsia used for the phylogenomic analyses 
-# ADD command line MUSCLE and Gblocks(Anna Maria, Tiago, can you complete this part please?)
+
+cp -r ./symbiont_phylo/OrthoFinder/Results*/Single_Copy_Orthologue_Sequences ## create a copy of the results directory containing the single copy panorthologs identified by OrthoFinder
+
+for i in Single_Copy_Orthologue_Sequences/*.fa; do muscle -in $i -out $i.aln; done ## align all the proteins in each cluster of single copy panorthologs
+for i in Single_Copy_Orthologue_Sequences/*.aln; do Gblocks $i -t=p; done ## remove hypervariable regions in each alignment
+
+# concatenate all the sequences in a single file: symbiont-phylo_concatenated.faa
+
 modeltest-ng -i symbiont-phylo_concatenated.faa -p 12 -T raxml -d aa
 raxmlHPC-PTHREADS -s Selected_AlnGblock.fasta -m model -n Selected_AlnGblock.fasta.boot -x 1234 -p 123 -f a -T 12 -# 100 
-# FLE : 
-# Midichloria : 
-# Rickttsia : PROTGAMMAIJTT
+# FLE : PROTGAMMAICPREV (CPREV+I+G)
+# Midichloria : PROTGAMMAILG (LG+I+G)
+# Rickettsia : PROTGAMMAIJTT (JTT+G)
